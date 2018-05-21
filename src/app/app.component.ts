e4696def70d5289deca713d283f6e67186975ead
秋天 <jkhhuse@126.com>
2018-05-21 22:57:59 +0800

    简单的checkbox实现
    待优化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'unit-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   dataSet = [
     // {
@@ -17,7 +17,29 @@ export class AppComponent {
     //   age: 2
     // }
   ];
+
   pageSize = 10;
   page = 1;
+  allChecked = false;
+
+  ngOnInit() {
+    for (let i = 0; i < 6; i++) {
+      this.dataSet.push({
+        name   : `Edward King ${i}`,
+        age    : 32,
+        checked: false
+      });
+    }
+  }
+
+  refreshStatus(): void {
+    const allChecked = this.dataSet.every(value => value.checked === true);
+    this.allChecked = allChecked;
+  }
+
+  checkAll(value: boolean): void {
+    this.allChecked = !this.allChecked;
+    this.dataSet.forEach(data => data.checked = this.allChecked);
+  }
 
 }
