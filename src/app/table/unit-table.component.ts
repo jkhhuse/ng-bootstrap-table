1d6981b11f799efa9a575ff31d67a26e674a97c1
秋天 <jkhhuse@126.com>
2018-08-06 10:19:05 +0800

    解决多个sort同时选中问题

--- src/app/table/unit-table.component.ts
+++ src/app/table/unit-table.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input, ViewEncapsulation,
-  TemplateRef, ContentChildren, QueryList } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation,
+  TemplateRef, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Subscription } from 'rxjs/Subscription';
+import { merge } from 'rxjs/operators/merge';
 import { UnitThComponent } from './unit-th.component';
 
 @Component({
@@ -8,15 +11,27 @@ import { UnitThComponent } from './unit-th.component';
   styleUrls: ['./unit-table.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class UnitTableComponent implements OnInit {
+export class UnitTableComponent implements OnInit, AfterContentInit  {
 
   @Input() dataSet: any;
   @Input() pageSize: string;
   @Input() page: string;
   @Input() unitLoading: boolean;
   @ContentChildren(UnitThComponent, { descendants: true }) listOfUnitThComponent: QueryList<UnitThComponent>;
+  @Output() nzSortChange = new EventEmitter<{ key: string, value: string }>();
+  private _singleSort = true;
+  private sortSubscription: Subscription;
   private _noResult: string | TemplateRef<void>;
   isNoResultString: boolean;
+
+  @Input()
+  set nzSingleSort(value: boolean) {
+    this._singleSort = value != null && `${value}` !== 'false';
+  }
+
+  get nzSingleSort(): boolean {
+    return this._singleSort;
+  }
 
   @Input()
   set nzNoResult(value: string | TemplateRef<void>) {
@@ -33,7 +48,21 @@ export class UnitTableComponent implements OnInit {
   ngOnInit() {
   }
 
-  pageChanged() {
+  ngAfterContentInit(): void {
+    let sortChange = new Subject<{ key: string, value: string }>().asObservable();
+    const listOfTh = this.listOfUnitThComponent.toArray();
+    const sortChangeArray = listOfTh.map(th => th.unitSortChangeWithKey);
+    if (sortChangeArray.length) {
+      sortChangeArray.forEach(sort => {
+        sortChange = sortChange.pipe(merge(sort.asObservable()));
+      });
+    }
+    this.sortSubscription = sortChange.subscribe(data => {
+      this.nzSortChange.emit(data);
+      if (this.nzSingleSort) {
+        listOfTh.forEach(th => th.unitSort = (th.unitSortKey === data.key ? th.unitSort : null));
+      }
+    });
   }
 
 }
